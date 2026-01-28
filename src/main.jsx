931d7e8ace847fdb1afa07bbc2f241c3126a9ec0
Beckett Evans <145697742+BeckettEvans@users.noreply.github.com>
2026-01-27 16:25:16 -0800

    Initialize main.jsx with React and PennyPilot component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PennyPilot from './components/PennyPilot.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PennyPilot />
+  </React.StrictMode>,
+);
